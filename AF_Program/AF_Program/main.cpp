4b97a47f82f02dad82ffd42fd24b686d2e32cb00
AndrewF001 <42306036+AndrewF001@users.noreply.github.com>
2021-09-26 15:32:29 +0100

    clean up

--- AF_Program/AF_Program/main.cpp
+++ AF_Program/AF_Program/main.cpp
@@ -1,20 +1,9 @@
 #include "MainWidget.h"
 #include <QtWidgets/QApplication>
-#include "Serilizer.h"
 
-class test {
-public:
-    int x;
-};
 
 int main(int argc, char *argv[])
 {
-    test* Original = new test();
-    Original->x = 1;
-    unsigned char* buffer = Serilizer::Safe_Serialize(Original);
-    test* copy = Serilizer::Safe_Deserialize<test>(buffer);
-    int y = copy->x;
-
     QApplication a(argc, argv);
     MainWidget w;
     w.show();
